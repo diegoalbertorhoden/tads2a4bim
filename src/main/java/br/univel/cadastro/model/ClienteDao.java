5d81889fd45a933f334885e8478d0df52e768f22
univel <univel@univel.br>
2015-10-13 20:22:38 -0300

    Implementação do H2

--- src/main/java/br/univel/cadastro/model/ClienteDao.java
+++ src/main/java/br/univel/cadastro/model/ClienteDao.java
@@ -1,14 +1,15 @@
 package br.univel.cadastro.model;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ClienteDao {
 	
 	public void inserir(Cliente c);
 	
-	public void atualizar(Cliente c);
+	public void atualizar(Cliente c) throws SQLException;
 	
-	public void excluir(Cliente c);
+	public void excluir(Cliente c) throws SQLException;
 	
 	public Cliente buscar(int id);
 	
---
+++ src/main/java/br/univel/cadastro/model/ClienteDaoH2Impl.java
@@ -0,0 +1,76 @@
+package br.univel.cadastro.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ClienteDaoH2Impl implements ClienteDao {
+	
+	private static Connection con;
+	
+	private Connection getConnection(){
+		
+		synchronized(con){
+			
+			if (con == null) {
+				// TODO
+			}
+			return con;
+			
+		}
+	}
+
+	@Override
+	public void inserir(Cliente c) {
+		String sql = "INSERT INTO....";
+		try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void atualizar(Cliente c) throws SQLException {
+		PreparedStatement ps = con.
+				prepareStatement("UPDATE PESSOA SET ID = ?, NOME = ? WHERE ID =?");
+		ps.setInt(1, 2);
+		ps.setString(2, "Hira");
+		ps.setInt(3, 1);
+		int res = ps.executeUpdate();
+
+
+		System.out.println(res + "registro atualizado");
+	}
+
+	@Override
+	public void excluir(Cliente c) throws SQLException {
+		PreparedStatement ps = con.prepareStatement("DELETE FROM PESSOA WHERE ID = ?");
+		ps.setInt(1, 2);
+		int res = ps.executeUpdate();
+
+		System.out.println(res + "registro deletado");
+
+	}
+
+	@Override
+	public Cliente buscar(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente buscarPorExemplo(Cliente c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
