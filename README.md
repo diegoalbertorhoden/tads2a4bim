abf13e9cf9c8afdbcf7f10dc55fbf85433fe1e5b
univel <univel@univel.br>
2015-10-13 19:49:10 -0300

    Commit Inicial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# tads2a4bim
+# Tecnologia em Análise e Desenvolvimento de Sistemas
+
+
+2º Ano, 4º Bimestre de 2015
+[Univel](http://www.univel.br)
+
+#13 de outubro de 2015
+
+* Criação do repositório
+* Criação do Projeto
+* Criação do Cliente e DAO.
---
+++ src/main/java/br/univel/cadastro/model/Uf.java
@@ -0,0 +1,18 @@
+package br.univel.cadastro.model;
+
+public enum Uf {
+
+	PR("Paraná"),
+	SP("São Paulo"),
+	SC("Santa Catarina");
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	
+	private Uf(String nome){
+		this.nome=nome;
+	}
+}
